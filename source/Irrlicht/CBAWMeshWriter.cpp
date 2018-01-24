4e7fac79f685f9ff7e3caff54397a431e2e3a819
Criss <crisserpl2@gmail.com>
2018-01-24 02:49:29 +0100

    *.baw exporter: Updated format design and writing code (materials)

--- source/Irrlicht/CBAWMeshWriter.cpp
+++ source/Irrlicht/CBAWMeshWriter.cpp
@@ -6,6 +6,8 @@
 #include "IWriteFile.h"
 #include "irrArray.h"
 #include "IMesh.h"
+#include "SVertexManipulator.h"
+#include "ITexture.h"
 
 namespace irr { namespace scene 
 {
@@ -28,43 +30,29 @@ namespace irr { namespace scene
 	bool CBAWMeshWriter::writeMesh(io::IWriteFile * _file, scene::ICPUMesh * _mesh, int32_t _flags)
 	{
 		//! @todo refactor (divide into few functions) writeMesh()
+		const uint32_t materialWithoutTexturesByteSize = 4 + 4*4 + 4 + 2*4 + 4 + 1 + 1 + 1 + 4 + 4 + 1;
+		const uint32_t materialTextureWithoutStringByteSize = 4 + 4;
 		const uint32_t headerByteSize = (1 + _mesh->getMeshBufferCount()) * (32/8); // mesh_cnt + mesh_cnt*mesh_offset
-		core::array<int32_t> header;
+		core::array<uint32_t> header;
 		header.reallocate(headerByteSize/(32/8));
 		header.push_back(_mesh->getMeshBufferCount());
 		header.push_back(headerByteSize); // offset of 0 submesh
 
-		for (uint32_t mbi = 0u; mbi < _mesh->getMeshBufferCount() - 1; ++mbi) // offsets of the rest of submeshes
-		{
-			const ICPUMeshBuffer* meshBuf = _mesh->getMeshBuffer(mbi);
-			const IMeshDataFormatDesc<core::ICPUBuffer>* desc = meshBuf->getMeshDataAndFormat();
-			
-			const core::ICPUBuffer *pbuf = desc->getMappedBuffer(meshBuf->getPositionAttributeIx()),
-				*tbuf = desc->getMappedBuffer(meshBuf->getTcoordAttributeIx()),
-				*nbuf = desc->getMappedBuffer(meshBuf->getNormalAttributeIx());
-			uint32_t submeshOffset = header[header.size() - 1] + pbuf->getSize();
-			if (tbuf != pbuf)
-				submeshOffset += tbuf->getSize();
-			if (nbuf != tbuf && nbuf != pbuf)
-				submeshOffset += nbuf->getSize();
-			submeshOffset += meshBuf->getIndexCount() * (meshBuf->getIndexType() == video::EIT_16BIT ? 2 : 4);
-			header.push_back(submeshOffset);
-		}
-		// end of header
-		_file->write(header.const_pointer(), header.size() * sizeof(*header.pointer()));
+		// TODO: make place in file for header which will be written in the end of the function
 
 		// prepare data of every submesh
 		for (uint32_t mbi = 0u; mbi < _mesh->getMeshBufferCount(); ++mbi)
 		{
 			const ICPUMeshBuffer* meshBuf = _mesh->getMeshBuffer(mbi);
 			const IMeshDataFormatDesc<core::ICPUBuffer>* desc = meshBuf->getMeshDataAndFormat();
+			const uint8_t indexSize = (meshBuf->getIndexType() == video::EIT_16BIT ? 2 : 4);
 			core::array<core::vector3df> pos;
 			core::array<core::vector2df> uvs;
-			core::array<core::vector4df_SIMD> normals; // or <int32_t>? INT_2_10_10_10_REV
+			core::array<int32_t> normals; // INT_2_10_10_10_REV format
 			const std::pair<void *, size_t> indices = // .first: ptr to indices; .second: size of indices in bytes
 				std::make_pair(
 					(uint8_t *)desc->getIndexBuffer()->getPointer() + meshBuf->getIndexBufferOffset(),
-					meshBuf->getIndexCount() * (meshBuf->getIndexType() == video::EIT_16BIT ? 2 : 4)
+					meshBuf->getIndexCount() * indexSize
 			);
 
 			size_t ix = 0u;
@@ -90,30 +78,31 @@ namespace irr { namespace scene
 				core::vectorSIMDf v;
 				if (meshBuf->getAttribute(v, meshBuf->getNormalAttributeIx(), ix++)) // if `ix` is out of buffer range
 					break;
-				normals.push_back(v);
+				normals.push_back(quantizeNormal2_10_10_10(v));
 			}
 
 			const uint32_t cnt[4]{ pos.size(), uvs.size(), normals.size(), meshBuf->getIndexCount() };
 			const uint32_t v_offset = 4*sizeof(*cnt),
 				uv_offset = v_offset + cnt[0] * sizeof(pos[0]),
 				vn_offset = uv_offset + cnt[1] * sizeof(uvs[0]),
-				idx_offset = vn_offset + cnt[2] * sizeof(normals);
-			const uint32_t offsets[4]{ v_offset, uv_offset, vn_offset, idx_offset }; // submesh-local offsets
+				idx_offset = vn_offset + cnt[2] * sizeof(normals[0]),
+				mat_offset = idx_offset + indices.second;
+			const uint32_t offsets[5]{ v_offset, uv_offset, vn_offset, idx_offset, mat_offset }; // submesh-local offsets
 
 			const video::SMaterial & mat = meshBuf->getMaterial();
-			uint8_t matFlags = 0u;
-			matFlags |= mat.Wireframe << 0;
-			matFlags |= mat.PointCloud << 1;
-			matFlags |= mat.ZWriteEnable << 2;
-			matFlags |= mat.BackfaceCulling << 3;
-			matFlags |= mat.FrontfaceCulling << 4;
-			matFlags |= mat.RasterizerDiscard << 5;
 			const uint32_t matColor[4] {
 				mat.AmbientColor.color,
 				mat.DiffuseColor.color,
 				mat.EmissiveColor.color,
 				mat.SpecularColor.color
 			};
+			uint8_t matFlags = 0u;
+			matFlags |= (mat.Wireframe << 0);
+			matFlags |= (mat.PointCloud << 1);
+			matFlags |= (mat.ZWriteEnable << 2);
+			matFlags |= (mat.BackfaceCulling << 3);
+			matFlags |= (mat.FrontfaceCulling << 4);
+			matFlags |= (mat.RasterizerDiscard << 5);
 
 			_file->write(cnt, sizeof(cnt));
 			_file->write(offsets, sizeof(offsets));
@@ -122,9 +111,59 @@ namespace irr { namespace scene
 			_file->write(uvs.const_pointer(), uvs.size() * sizeof(uvs[0]));
 			_file->write(normals.const_pointer(), normals.size() * sizeof(normals[0]));
 			_file->write(indices.first, indices.second);
+			_file->write(&indexSize, 1);
 
-			// get material texture mat.getTexture(n), dynamic_cast it to ITexture, .getName() and possibly use NamedPath::PathToName on that name (will get path object)
+			_file->write(&mat.MaterialType, sizeof(video::E_MATERIAL_TYPE));
+			_file->write(matColor, sizeof(matColor));
+			_file->write(&mat.Shininess, 4);
+			_file->write(&mat.MaterialTypeParam, 4);
+			_file->write(&mat.MaterialTypeParam2, 4);
+			_file->write(&mat.Thickness, 4);
+			_file->write(&mat.ZBuffer, 1);
+			const uint8_t colorMask = mat.ColorMask;
+			_file->write(&colorMask, 1);
+			const uint8_t blendOp = mat.BlendOperation;
+			_file->write(&blendOp, 1);
+			_file->write(&mat.PolygonOffsetConstantMultiplier, 4);
+			_file->write(&mat.PolygonOffsetGradientMultiplier, 4);
+			_file->write(&matFlags, 1);
+
+			uint32_t submeshOffset = header.getLast();
+			submeshOffset += materialWithoutTexturesByteSize;
+
+			for (int i = 0; i < video::MATERIAL_MAX_TEXTURES; ++i) {
+				io::path path;
+				if (video::ITexture * texture = dynamic_cast<video::ITexture *>(mat.getTexture(i))) {
+					path = texture->getName().getInternalName();
+					const size_t len = std::strlen(path.c_str()) + 1;
+					_file->write(path.c_str(), len);
+					submeshOffset += len;
+				}
+				else {
+					int8_t zero = 0;
+					_file->write(&zero, 1);
+					++submeshOffset;
+				}
+				uint32_t params = 0u;
+				params |= mat.TextureLayer[i].SamplingParams.SeamlessCubeMap;
+				(params <<= 5) |= mat.TextureLayer[i].SamplingParams.AnisotropicFilter;
+				(params <<= 1) |= mat.TextureLayer[i].SamplingParams.UseMipmaps;
+				(params <<= 1) |= mat.TextureLayer[i].SamplingParams.MaxFilter;
+				(params <<= 3) |= mat.TextureLayer[i].SamplingParams.MinFilter;
+				(params <<= 3) |= mat.TextureLayer[i].SamplingParams.TextureWrapW;
+				(params <<= 3) |= mat.TextureLayer[i].SamplingParams.TextureWrapV;
+				(params <<= 3) |= mat.TextureLayer[i].SamplingParams.TextureWrapU;
+
+				_file->write(&params, 4);
+				_file->write(&mat.TextureLayer[i].SamplingParams.LODBias, 4);
+
+				submeshOffset += materialTextureWithoutStringByteSize;
+			}
+
+			if (header.size() < headerByteSize/(32 / 8))
+				header.push_back(submeshOffset);
 		}
+		// TODO write header on position 0 in file (previosly reserved place)
 	}
 
 }} // end ns irr::scene
--- source/Irrlicht/CBAWMeshWriter.h
+++ source/Irrlicht/CBAWMeshWriter.h
@@ -6,14 +6,32 @@
 /*
 Format design:
 --------------
-submeshes_cnt: int32
-submeshes_offsets: int32
-submeshes:
-	def submesh:
-		v_cnt, vt_cnt, vn_cnt, fc_cnt : int32
-		offsets v, vt, vn, idx : int32 #offsets relative to local submesh
-		blocks v(float3), vt(float2), vn(INT_2_10_10_10_REV/float3), idx(uint8/uint16)
-#todo materials
+submeshes_cnt: uint32
+submeshes_offsets : uint32[submeshes_cnt]
+def submesh:
+	v_cnt, vt_cnt, vn_cnt, fc_cnt : 4*uint32
+	offsets v, vt, vn, idx, mat : 5*uint32 # offsets relative to local submesh
+	blocks v(float3), vt(float2), vn(int32, INT_2_10_10_10_REV), idx(uint16|uint32)
+	index_size : uint8 # must be 2 or 4
+	def material
+		type : int32
+		colors : int32[4] #order: ambient, diffuse, emissive, specular (each color represented by 32bits)
+		shininess : float
+		params : float[2]
+		thickness : float
+		zbuffer : uint8
+		color_mask : uint8
+		blend_op : uint8
+		polygon_offset_const_mltr : float
+		polygon_offset_gradient_mltr : float
+		flags : uint8 (6 bits, from lsb: wireframe, pointcloud, zwrite_enable, backface_culling, frontface_culling, rasterizer_discard)
+		def texture:
+			path : cstring (0-byte terminated)
+			params : uint32 (20 bits, from lsb: wrapu:3, wrapv:3, wrapw:3, minfltr:3, maxfltr:1, use_mm:1, anisofltr:5, seamlesscubemap:1)
+			lod_bias : float
+		textures : texture[_IRR_MATERIAL_MAX_TEXTURES_]
+	mat : material
+submeshes : submesh[submeshes_cnt]
 */
 
 namespace irr { 
