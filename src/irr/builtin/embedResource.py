a28ea8dc06de94178774173681413f850103fa73
Hazardu <42750285+Hazardu@users.noreply.github.com>
2020-03-12 14:37:26 +0100

    namespace irr/builtin added to embedded resources

--- src/irr/builtin/embedResource.py
+++ src/irr/builtin/embedResource.py
@@ -11,13 +11,16 @@ import sys
 # for regex to truncate extensions
 import re
 
+print("Script working")
+
 if  len(sys.argv) < 2 :
     print("Incorrect argument count")
 else:
 
     #arguments
     inputfilename = sys.argv[1]
-    outputfilename = inputfilename + '.h'
+    varname = sys.argv[2]
+    outputfilename = sys.argv[3]
     #outputfilename = re.sub(r'\..+', '', inputfilename)+ '.h'
 
     #file size
@@ -26,10 +29,11 @@ else:
     #opening a file
     outp = open(outputfilename,"w+")
 
+    print("Created file at %s" % outputfilename)
     outp.write("#include <stdlib.h>\n")
 
     # outp.write("const uint8_t* resource[%d] = {\n" % fsize) # array size is not required
-    outp.write("const uint8_t* resource[] = {\n")
+    outp.write("namespace irr { \n\tnamespace builtin { \n\t\tconst uint8_t* %s[] = {\n" % varname)
 
     with open(inputfilename, "rb") as f:
         index = 0
@@ -46,6 +50,6 @@ else:
         # if fsize != index :
         # print("Error, incorrect size of file and records written onto an array")
     
-    outp.write( "};\n")
-    outp.write("const size_t resource_len = sizeof(resource);\n\n")
+    outp.write( "\t\t};\n")
+    outp.write("\t\tconst size_t %s_len = sizeof(%s);\n\t}\n}" % varname,varname)
     outp.close()
